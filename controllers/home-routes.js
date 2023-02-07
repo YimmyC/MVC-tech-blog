370c2f8e0f2458a7fea9b95fc521641f3056e406
YimmyC <jim.cassano1@gmail.com>
2023-02-07 15:25:52 -0600

    login logout functionality working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const withAuth = require("./../utils/auth");
 
 // route to get all dishes
 router.get("/", async (req, res) => {
@@ -7,7 +8,16 @@ router.get("/", async (req, res) => {
     res.json(err);
   });
   const posts = postData.map((post) => post.get({ plain: true }));
-  res.render("all", { posts });
+  res.render("all", { posts, loggedIn: req.session.loggedIn });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
